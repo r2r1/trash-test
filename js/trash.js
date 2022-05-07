c09776dcb71ceda75648c45ae3030133f3bf8a00
GrigorY <99581365+r2r1@users.noreply.github.com>
2022-05-07 19:19:31 +0300

    Update trash.js

--- js/trash.js
+++ js/trash.js
@@ -7,11 +7,12 @@ function  getInfo() {
 	const url = "/trashList.json"
 	let response = await fetch(url);
 
-	if (response.ok) { // если HTTP-статус в диапазоне 200-299
+		if (response.ok) { // если HTTP-статус в диапазоне 200-299
   // получаем тело ответа (см. про этот метод ниже)
-  	let json = await response.json();
-	} else {
-  	alert("Ошибка HTTP: " + response.status);
+  			let json = await response.json();
+	}
+		else {
+  			alert("Ошибка HTTP: " + response.status);
 	}
 }
 
